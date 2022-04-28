a9cb2ad976cee3e2a10345f3b1ea154f0f895e21
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-04-28 17:19:48 +0530

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+
 import profile_pic from "../../Assets/profile_pic.png"
 import Tilt from "react-parallax-tilt";
 import {
