6eb862f616d3187a21e0279986dd1e9836b9db7b
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-05-01 11:26:14 +0530

    some updates

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -16,6 +16,10 @@ function ProjectCards(props) {
           <BiLinkExternal /> &nbsp;
           {props.isBlog ? "View Blog" : "View Project"}
         </Button>
+        <Button variant="primary" href={props.live_link}>
+          <BiLinkExternal /> &nbsp;
+          {props.isBlog ? "View Blog" : "View Live"}
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.jpeg";
+// import editor from "../../Assets/Projects/codeEditor.png";
+import mednear from "../../Assets/Projects/mednear.png";
+import kfc from "../../Assets/Projects/kfc.PNG";
+import onbikes from "../../Assets/Projects/onbikes.png";
 
 function Projects() {
   return (
@@ -23,35 +23,38 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={mednear}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="Mednear Clone"
+              description="Mednear is the Primary Online Pharmacy in India that provides prescription, Over-the-counter medicines, and generic medicines all over India. The main motive of MedNear is to Ensuring Health for all. They make a simple way for people to Buy Prescription Medicine Online from anywhere in India and any time at the best prices."
+              link="https://github.com/HrgChampion/Mednear-clone"
+              live_link="mednear.netlify.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={onbikes}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Onn-Bikes Clone"
+              description="Whenever I go to a new city where I don’t know anyone or never been before it gets me little scared than, I really felt like I need my own bike to explore the city and go wherever I need, that’s exactly where our website will help you. it provides you every kind of bike whatever you’re looking for. Cruising, Sports, even a Scooter is available. you just need a driving license number and you’re ready to go. yeah! it is that simple."
+              link="https://github.com/Ritesh6997/ONN-BIke-Complete-Project"
+              live_link="sharp-brahmagupta-a8b2e3.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={kfc}
               isBlog={false}
-              title="Editor.io"
+              title="KFC Clone"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              link="https://github.com/roohitgupta/KFC_Clone"
+              live_link=""
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
@@ -59,9 +62,9 @@ function Projects() {
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               link="https://github.com/soumyajit4419/Plant_AI"
             />
-          </Col>
+          </Col> */}
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
@@ -69,9 +72,9 @@ function Projects() {
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
               link="https://github.com/soumyajit4419/AI_For_Social_Good"
             />
-          </Col>
+          </Col> */}
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
@@ -80,7 +83,7 @@ function Projects() {
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
